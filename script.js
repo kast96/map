4909f9e06dd31e44ae92d1fd0ec08e9b691d41c8
kast96 <kast96@yandex.ru>
2025-07-10 09:10:01 +0300

    remove quests

--- script.js
+++ script.js
@@ -880,11 +880,27 @@ document.addEventListener('DOMContentLoaded', function () {
 					}, 'textarea');
 				});
 
+				const questRemoveEl = document.createElement('button');
+				questRemoveEl.className = 'task-remove task-right-button';
+				questRemoveEl.setAttribute('data-quest-id', quest.id);
+				questRemoveEl.innerHTML = `
+					<svg class='task-remove-icon task-right-button-icon' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+						<path d="M135.2 17.7L128 32 32 32C14.3 32 0 46.3 0 64S14.3 96 32 96l384 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-96 0-7.2-14.3C307.4 6.8 296.3 0 284.2 0L163.8 0c-12.1 0-23.2 6.8-28.6 17.7zM416 128L32 128 53.2 467c1.6 25.3 22.6 45 47.9 45l245.8 0c25.3 0 46.3-19.7 47.9-45L416 128z"/>
+					</svg>
+				`;
+				questHeaderEl.appendChild(questRemoveEl);
+
+				questRemoveEl.addEventListener('click', function(){
+					if (!confirm('Точно удалить задание?')) return;
+					store.quests[tabType].splice(index, 1);
+					renderQuests();
+				});
+
 				questEl.appendChild(questHeaderEl);
 
 				const tasksContainer = document.createElement('div');
 				tasksContainer.className = 'quest-body';
-				quest.tasks.forEach(task => {
+				quest.tasks.forEach((task, indexTask) => {
 					const taskEl = document.createElement('div');
 					taskEl.className = `task-item ${task.status}`;
 
@@ -964,14 +980,34 @@ document.addEventListener('DOMContentLoaded', function () {
 					taskRightEl.className = 'task-right';
 
 					if (checkLocation(task.location)) {
-						taskRightEl.innerHTML = `
-							<button class='show-location' data-task-id='${task.id}' data-quest-id='${quest.id}'>
-								<svg class='show-location-icon' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
-									<path d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"/>
-								</svg>
-							</button>
+						const taskShowLocationEl = document.createElement('button');
+						taskShowLocationEl.className = 'show-location task-right-button';
+						taskShowLocationEl.setAttribute('data-task-id', task.id);
+						taskShowLocationEl.setAttribute('data-quest-id', quest.id);
+						taskShowLocationEl.innerHTML = `
+							<svg class='show-location-icon task-right-button-icon' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+								<path d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"/>
+							</svg>
 						`;
+						taskRightEl.appendChild(taskShowLocationEl);
 					}
+
+					const taskRemoveEl = document.createElement('button');
+					taskRemoveEl.className = 'task-remove task-right-button';
+					taskRemoveEl.setAttribute('data-task-id', task.id);
+					taskRemoveEl.setAttribute('data-quest-id', quest.id);
+					taskRemoveEl.innerHTML = `
+						<svg class='task-remove-icon task-right-button-icon' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+							<path d="M135.2 17.7L128 32 32 32C14.3 32 0 46.3 0 64S14.3 96 32 96l384 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-96 0-7.2-14.3C307.4 6.8 296.3 0 284.2 0L163.8 0c-12.1 0-23.2 6.8-28.6 17.7zM416 128L32 128 53.2 467c1.6 25.3 22.6 45 47.9 45l245.8 0c25.3 0 46.3-19.7 47.9-45L416 128z"/>
+						</svg>
+					`;
+					taskRightEl.appendChild(taskRemoveEl);
+
+					taskRemoveEl.addEventListener('click', function(){
+						if (!confirm('Точно удалить подзадачу?')) return;
+						quest.tasks.splice(indexTask, 1);
+						renderQuests();
+					});
 
 					taskEl.appendChild(taskRightEl);
 
